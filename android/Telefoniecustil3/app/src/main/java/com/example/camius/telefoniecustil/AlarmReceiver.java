99503c0515e928406976506bffadd3082d350170
OnuCameliaW <onu.camelia@yahoo.com>
2017-04-03 22:18:25 +0300

    Commit

---
+++ android/Telefoniecustil3/app/src/main/java/com/example/camius/telefoniecustil/AlarmReceiver.java
@@ -0,0 +1,38 @@
+package com.example.camius.telefoniecustil;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm received!", Toast.LENGTH_LONG).show();
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        callIntent.setData(Uri.parse("tel:0748680907"));
+        context.getApplicationContext().startActivity(callIntent);
+    }
+
+}
+
---
+++ android/Telefoniecustil3/app/src/main/java/com/example/camius/telefoniecustil/MainActivity.java
@@ -0,0 +1,226 @@
+package com.example.camius.telefoniecustil;
+
+import java.util.Calendar;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.app.Dialog;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    final Context context = this;
+    private static final int RESULT_PICK_CONTACT = 85500;
+    private TextView textView1;
+    private TextView textView2;
+
+    TimePicker myTimePicker;
+    Button buttonSetDialog;
+    Button buttonPickContact;
+    Button buttonAddAlarm;
+    TextView textAlarmPrompt;
+
+    TimePickerDialog timePickerDialog;
+
+    final static int RQS_1 = 1;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View startAlarm = getLayoutInflater().inflate(R.layout.setalarm, null);
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView2 = (TextView) findViewById(R.id.textView2);
+
+        // add PhoneStateListener
+        PhoneCallListener phoneListener = new PhoneCallListener();
+        TelephonyManager telephonyManager = (TelephonyManager) this
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(phoneListener,PhoneStateListener.LISTEN_CALL_STATE);
+
+        textAlarmPrompt = (TextView)findViewById(R.id.alarmprompt);
+        buttonAddAlarm = (Button)findViewById(R.id.addAlarm);
+
+
+
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.setalarm);
+        buttonAddAlarm.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                dialog.show();
+            }});
+
+
+        buttonPickContact = (Button)dialog.findViewById(R.id.pickContact);
+        buttonSetDialog = (Button)dialog.findViewById(R.id.startSetDialog);
+
+        buttonSetDialog.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                textAlarmPrompt.setText("");
+                openTimePickerDialog(false);
+            }});
+        buttonPickContact.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+
+            }});
+    }
+//    public void pickContact(View v)
+//    {
+//        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+//                ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+//        startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+//    }
+    private void openTimePickerDialog(boolean is24r){
+        Calendar calendar = Calendar.getInstance();
+        timePickerDialog = new TimePickerDialog(
+                MainActivity.this,
+                onTimeSetListener,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                is24r);
+        timePickerDialog.setTitle("Set Alarm Time");
+
+        timePickerDialog.show();
+
+    }
+
+    OnTimeSetListener onTimeSetListener
+            = new OnTimeSetListener(){
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+            Calendar calNow = Calendar.getInstance();
+            Calendar calSet = (Calendar) calNow.clone();
+
+            calSet.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            calSet.set(Calendar.MINUTE, minute);
+            calSet.set(Calendar.SECOND, 0);
+            calSet.set(Calendar.MILLISECOND, 0);
+
+            if(calSet.compareTo(calNow) <= 0){
+                //Today Set time passed, count to tomorrow
+                calSet.add(Calendar.DATE, 1);
+            }
+
+            setAlarm(calSet);
+        }};
+
+    private void setAlarm(Calendar targetCal){
+        textAlarmPrompt.setText(
+                "\n\n***\n"
+                        + "Alarm is set@ " + targetCal.getTime() + "\n"
+                        + "***\n");
+
+        Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), RQS_1, intent, 0);
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(), pendingIntent);
+
+    }
+    private class PhoneCallListener extends PhoneStateListener {
+
+        private boolean isPhoneCalling = false;
+
+        String LOG_TAG = "LOGGING 123";
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            if (TelephonyManager.CALL_STATE_RINGING == state) {
+                // phone ringing
+                Log.i(LOG_TAG, "RINGING, number: " + incomingNumber);
+            }
+
+            if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+                // active
+                Log.i(LOG_TAG, "OFFHOOK");
+
+                isPhoneCalling = true;
+            }
+
+            if (TelephonyManager.CALL_STATE_IDLE == state) {
+                // run when class initial and phone call ended,
+                // need detect flag from CALL_STATE_OFFHOOK
+                Log.i(LOG_TAG, "IDLE");
+
+                if (isPhoneCalling) {
+
+                    Log.i(LOG_TAG, "restart app");
+
+                    // restart app
+                    Intent i = getBaseContext().getPackageManager()
+                            .getLaunchIntentForPackage(
+                                    getBaseContext().getPackageName());
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+
+                    isPhoneCalling = false;
+                }
+
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // check whether the result is ok
+        if (resultCode == RESULT_OK) {
+            // Check for the request code, we might be usign multiple startActivityForReslut
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Log.e("MainActivity", "Failed to pick contact");
+        }
+    }
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+        try {
+            String phoneNo = null ;
+            String name = null;
+            // getData() method will have the Content Uri of the selected contact
+            Uri uri = data.getData();
+            //Query the content uri
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            // column index of the phone number
+            int  phoneIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            // column index of the contact name
+            int  nameIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            phoneNo = cursor.getString(phoneIndex);
+            name = cursor.getString(nameIndex);
+            // Set the value to the textviews
+            textView1.setText(name);
+            textView2.setText(phoneNo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
